bd2fa8895598719350fb9a77cdfd2f15e98c7233
KapilPrevo <kapilmohodprevoyance@gmail.com>
2024-05-29 12:46:49 +0530

    add sample page

---
+++ lib/sample.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class Sample extends StatelessWidget {
+  const Sample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Data"),
+      ),
+    );
+  }
+}
